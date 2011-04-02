85a1d0ad861d42f7217087e4c3c724fb85f29eb0
Matt Venn <matt@mattvenn.net>
2011-04-02 15:09:19 +0100

    added project to git

---
+++ boxmaze.py
@@ -0,0 +1,79 @@
+# thanks to kellbot for info on the sdxf library: http://www.kellbot.com/sdxf-python-library-for-dxf/
+import sdxf
+dxfdir="processDXF/"
+# globals
+thickness = 3.72 #thickness of material
+laserBurnGap = 0.3 #depends on laser cutter
+boxWidth = 100
+boxLength = 140
+
+cutColor = 0
+etchColor = 2
+patternColor = 3
+
+def drawRef():
+    linePoints = [(-2,0),(2,0)]
+    d.append(sdxf.Line(points=linePoints,color=cutColor)) #flag=1 means close the polyline
+    linePoints = [(0,-2),(0,2)]
+    d.append(sdxf.Line(points=linePoints,color=cutColor)) #flag=1 means close the polyline
+
+def drawHinge(x,y):
+    d.append(sdxf.Circle(center=(x+10,y+thickness * 5.5),radius=2,color=cutColor))
+    d.append(sdxf.Arc(center=(x+10,y+thickness * 8 - 10),radius=10,startAngle=0,endAngle=180,color=cutColor))
+    linePoints = [(x+0,y+thickness * 8 - 10 ),(x+0,y+0),(x+20,y+0),(x+20,y+thickness * 8 - 10)]
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor)) 
+
+def drawCatch(x,y):
+    notchDepth = 3
+    linePoints = [(x+0,y+0),(x+30,y+0),(x+30,y+thickness + laserBurnGap ),(x+20,y+thickness + laserBurnGap ),(x+20,y+thickness * 7 ),(x+10,y+thickness * 7 ),
+    #notch
+    (x+10,y+thickness * 6 - laserBurnGap),(x+10 + notchDepth,y+ thickness * 6 - laserBurnGap),(x+10 + notchDepth,y+ thickness * 5),(x+10, y+thickness * 5 ),
+    #finish
+    (x+10,y+thickness + laserBurnGap ),(x+0,y+ thickness + laserBurnGap),
+    ]
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor,flag=1)) 
+
+#give x and y for top left corner of box
+def drawHingeSlot(x,y):
+    linePoints = [(x+boxLength,y+10),(x+boxLength-20,y+10),(x+boxLength-20,y+10+thickness),(x+boxLength,y+10+thickness)]
+    #d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor)) 
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor,flag=1)) 
+    linePoints = [(x+boxLength,y+boxWidth-10),(x+boxLength-20,y+boxWidth-10),(x+boxLength-20,y+boxWidth-10-thickness),(x+boxLength,y+boxWidth-10-thickness)]
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor,flag=1)) 
+#    d.append(sdxf.Line(points=linePoints,color=cutColor)) 
+
+#give x and y for top left corner of box
+#slightly narrower for a tight fit
+def drawHingeSlotLid(x,y):
+    linePoints = [(x+boxLength,y+10+laserBurnGap),(x+boxLength-20,y+10+laserBurnGap),(x+boxLength-20,y+10+thickness-laserBurnGap),(x+boxLength,y+10+thickness-laserBurnGap)]
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor,flag=1)) 
+#    d.append(sdxf.Line(points=linePoints,color=cutColor)) 
+    linePoints = [(x+boxLength,y+boxWidth-10-laserBurnGap),(x+boxLength-20,y+boxWidth-10-laserBurnGap),(x+boxLength-20,y+boxWidth-10-thickness+laserBurnGap),(x+boxLength,y+boxWidth-10-thickness+laserBurnGap)]
+    #d.append(sdxf.Line(points=linePoints,color=cutColor)) #flag=1 means close the polyline
+    d.append(sdxf.LwPolyLine(points=linePoints,color=cutColor,flag=1)) #flag=1 means close the polyline
+
+def drawCatchSlot1(x,y):
+    linePoints = [(x+15-thickness/2+laserBurnGap,y+40),(x+15+thickness/2-laserBurnGap,y+40),(x+15+thickness/2-laserBurnGap,y+40+30),(x+15-thickness/2+laserBurnGap,y+40+30)]
+    #d.append(sdxf.Line(points=linePoints,color=cutColor)) 
+    d.append(sdxf.LwPolyLine(points=linePoints,flag=1,color=cutColor)) 
+
+def drawCatchSlot2(x,y):
+    linePoints = [(x+15-thickness/2+laserBurnGap,y+40+10),(x+15+thickness/2-laserBurnGap,y+40+10),(x+15+thickness/2-laserBurnGap,y+40+20),(x+15-thickness/2+laserBurnGap,y+40+20)]
+    d.append(sdxf.LwPolyLine(points=linePoints,flag=1,color=cutColor)) 
+    #d.append(sdxf.Line(points=linePoints,color=cutColor)) 
+
+d=sdxf.Drawing()
+drawRef()
+drawHinge(200,40)
+drawHinge(225,40)
+drawCatch(60,40)
+drawCatchSlot1(boxLength*1,boxWidth*3)
+drawCatchSlot2(boxLength*1,0)
+for col in range(2):
+    for row in range(4):
+        if row == 3:
+            drawHingeSlotLid(boxLength*col,boxWidth*row)
+        else:
+            drawHingeSlot(boxLength*col,boxWidth*row)
+
+d.saveas(dxfdir+'boxmaze.dxf')
---
+++ joinDXF.py
@@ -0,0 +1,37 @@
+import re
+dxfdir = "processDXF/"
+staticdxfdir = "staticDXF/"
+outdir="outputDXF/"
+
+headerFile = open(staticdxfdir + "header.dxf", "r")
+header = headerFile.read()
+footerFile = open(staticdxfdir + "footer.dxf", "r")
+footer = footerFile.read()
+
+#FILES = [ "boxmaze.dxf" ]
+FILES = [ "boxmaze.dxf" , "maze.dxf", "etchmazecolour.dxf", "boxmazeoutline.dxf" ]
+entities = ""
+for file in FILES:
+    filename = dxfdir + file
+    capture = False
+    print "processing " + filename
+    #entities = entities + "999\nimport from " + file + "\n"
+    infile = open( filename, "r" )
+    data = infile.readlines()
+
+    for i in range(len(data)):
+        if i < len(data) - 1:
+            m = re.search( "^ENDSEC$", data[i+1] )
+            if m != None:
+                capture = False
+        if capture:
+            entities = entities + data[i]
+        m = re.search( "^ENTITIES$", data[i] )
+        if m != None:
+            capture = True
+
+final = open( outdir + "boxmazefinal.dxf", "w" )
+final.write(header)
+final.write(entities)
+final.write(footer)
+print "finished!"
