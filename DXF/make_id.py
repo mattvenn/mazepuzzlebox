01af506e3944255f98e3897fd0414e2c463b4e96
Matt Venn <matt@mattvenn.net>
2011-05-08 20:26:54 +0100

    create dxf numbers

---
+++ DXF/make_id.py
@@ -0,0 +1,97 @@
+import sdxf
+import sys
+import math
+dxfdir="processDXF/"
+
+# globals
+if len(sys.argv) != 2:
+    print "give id on command line"
+    sys.exit(1)
+id = sys.argv[1]
+
+print "using id %s" % id
+
+char_width = 3
+char_height = 3
+char_spacing = 1
+char_color = 3
+x_offset = 183
+y_offset = 220
+#shift routine
+def shift(shift_x,shift_y,array):
+    angle = - math.pi / 2
+    for i in range(len(array)):
+        x = array[i][0]
+        y = array[i][1]
+        x = x + shift_x
+        y = y + shift_y
+        rotX = x * math.cos(angle) - y * math.sin(angle)
+        rotY = x * math.sin(angle) + y * math.cos(angle)
+        array[i] = (rotX + x_offset, rotY + y_offset)
+
+    return array
+
+#char defs
+def create_num(x,y,n):
+    if n == 0:
+        linePoints = [(0,0),(char_width,0),(char_width,char_height),(0,char_height),(0,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 1:
+        linePoints = [(0,0),(char_width,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+        linePoints = [(char_width/2,0),(char_width/2,char_height),(0,char_height)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 2:
+        linePoints = [(0,char_height),(char_width,char_height),(char_width,char_height/2),(0,char_height/2),(0,0),(char_width,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 3:
+        linePoints = [(0,char_height),(char_width,char_height),(char_width,0),(0,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+        linePoints = [(0,char_height/2),(char_width,char_height/2)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 4:
+        linePoints = [(0,char_height),(0,char_height/2),(char_width,char_height/2)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+        linePoints = [(char_width,char_height),(char_width,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 5:
+        linePoints = [(char_width,char_height),(0,char_height),(0,char_height/2),(char_width,char_height/2),(char_width,0),(0,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 6:
+        linePoints = [(0,char_height),(0,0),(char_width,0),(char_width,char_height/2),(0,char_height/2)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 7:
+        linePoints = [(0,char_height),(char_width,char_height),(char_width,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 8:
+        linePoints = [(0,0),(char_width,0),(char_width,char_height),(0,char_height),(0,0)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+        linePoints = [(0,char_height/2),(char_width,char_height/2)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+    if n == 9:
+        linePoints = [(char_width,0),(char_width,char_height),(0,char_height),(0,char_height/2),(char_width,char_height/2)]
+        linePoints = shift(x,y,linePoints)
+        d.append(sdxf.LwPolyLine(points=linePoints,color=char_color))
+
+#do it
+d=sdxf.Drawing()
+for i in range( len( id )):
+    print id[i]
+    x = i * char_width + char_spacing * i
+    create_num( x, 0, int(id[i]) )
+
+d.saveas(dxfdir+'id_numbers.dxf')
+    
