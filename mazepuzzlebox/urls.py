f682b9ea16035912d24a6d4e72c499d9aeb3d209
Matt Venn <matt@mattvenn.net>
2011-05-18 17:20:32 +0100

    added admin

--- mazepuzzlebox/urls.py
+++ mazepuzzlebox/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
     (r'^/?$', 'createbox.views.index'),
     (r'^create$', 'createbox.views.create'),
@@ -10,5 +11,5 @@ urlpatterns = patterns('',
     (r'^boxes/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/matthew/work/python/mazepuzzlebox/boxDXFs'}),
     (r'^mazePNGs/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/matthew/work/python/mazepuzzlebox/mazePNGs/'}),
     (r'^external/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/matthew/work/python/mazepuzzlebox/external/'}),
-
+    (r'^admin/', include(admin.site.urls)),
 )
