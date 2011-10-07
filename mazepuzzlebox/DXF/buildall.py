ab80543537bbd367420aa00f70070594c9bbc8c6
Matt Venn <matt@mattvenn.net>
2011-10-07 17:01:27 +0100

    worked out how to run django stuff as shell scripts - nearly

--- mazepuzzlebox/DXF/buildall.py
+++ mazepuzzlebox/DXF/buildall.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 import drawMaze
-import make_pieces
 import make_id
+import make_pieces
 import joinDXF
-
-jstr = '[[1,1,1,1,0,0],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,1,1],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,1,1],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,1,1,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]'
-drawMaze.drawMaze( jstr )
-make_pieces.make_pieces(4)
-make_id.make_id(401)
-joinDXF.joinDXF(100)
+from mazepuzzlebox.createbox.models import Box
+#jstr = '[[1,1,1,1,0,0],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,1,1],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,0,0,1,1,1],[0,0,0,1,0,0],[0,0,0,1,0,0],[0,1,1,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]'
+id = 100
+thickness=4
+make_pieces.make_pieces(float(thickness))
+"""
+DXF.drawMaze.drawMaze( box.maze,box )
+DXF.make_id.make_id(box.id)
+DXF.joinDXF.joinDXF(box.id)
+"""
--- mazepuzzlebox/DXF/buildall.sh
+++ mazepuzzlebox/DXF/buildall.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # boxid, thickness, json maze description
-cd ~/work/python/mazepuzzlebox/DXF/
-rm processDXF/*
-cp ~/work/lasercuts/boxmaze\ param\ v1-4.dxf processDXF/boxmazeoutline.dxf
-#give thickness
-python make_pieces.py $2
-#give json
-python drawMaze.py $3
-#give id
-python make_id.py $1
-#give id
-python joinDXF.py $1
+export DJANGO_SETTINGS_MODULE=mazepuzzlebox.settings
+export PYTHONPATH=~/work/python/mazepuzzlebox
 
+cd ~/work/python/mazepuzzlebox/DXF/
+python buildall.py
