d7642018ea1f9c1ec2dbc86014d82d843c0b65f4
Matt Venn <matt@mattvenn.net>
2011-11-06 18:44:54 +0000

    util for getting news from my website

---
+++ mazepuzzlebox/createbox/RSS.py
@@ -0,0 +1,26 @@
+import feedparser
+feed = 'http://www.mattvenn.net/feed/rss/?tag=mazepuzzlebox'
+from datetime import date
+def getLatestNews():
+    ret = []
+    channels = feedparser.parse(feed)
+
+    url = ''
+    summary = ''
+    title = ''
+    datepub = ''
+    for entry in channels.entries:
+        try:
+            url = unicode(entry.link, channels.encoding)
+            summary = unicode(entry.description, channels.encoding)
+            title = unicode(entry.title, channels.encoding)
+            #datepub = unicode(entry.date, channels.encoding)
+        except:
+            url = entry.link
+            summary = entry.description
+            title = entry.title
+            #datepub = entry.date
+    
+            ret.append( { 'url' : url, 'summary' : summary, 'title' : title , 'date' : datepub } )
+
+    return ret[0:2]
