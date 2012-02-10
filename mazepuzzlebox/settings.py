537f4d10e3173f8ffd5aa2349f60be373d4cd127
Admin <admin@admins-macbook-pro-90.local>
2012-02-10 20:45:03 +0000

    changes for gav laptop

--- mazepuzzlebox/settings.py
+++ mazepuzzlebox/settings.py
@@ -14,7 +14,7 @@ if LIVE:
     DEBUG = False
     ADMIN_MEDIA_PREFIX = '/media/admin/'
 else:
-    ROOT_DIR = "/home/matthew/work/python/mazepuzzlebox/"
+    ROOT_DIR = "/Users/admin/Documents/Maze Puzzle Box/mazepuzzlebox/"
     ROOT_URLCONF = 'mazepuzzlebox.urls'
     DEBUG = True
     ADMIN_MEDIA_PREFIX = '/media/'
