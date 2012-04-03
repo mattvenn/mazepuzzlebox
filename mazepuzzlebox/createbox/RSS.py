7e636f94f34a51e6c6f6902c23cfe89c3df2840a
Matt Venn <matt@mattvenn.net>
2012-04-03 11:59:26 +0100

    using rss2 to get dates, and caching for 1 hour

--- mazepuzzlebox/createbox/RSS.py
+++ mazepuzzlebox/createbox/RSS.py
@@ -1,10 +1,11 @@
 import sys
 import shelve
+import time
 from feedcache import cache
 from datetime import date
 
-feed = 'http://www.mattvenn.net/feed/rss/?tag=mazepuzzlebox'
-
+feed = 'http://www.mattvenn.net/feed/rss2/?tag=mazepuzzlebox'
+ttl = 60 * 60 #1 hour
 def getLatestNews():
     ret = []
     storage = shelve.open('.feedcache' )
@@ -13,21 +14,21 @@ def getLatestNews():
     title = ''
     datepub = ''
     try:
-        fc = cache.Cache(storage)
+        fc = cache.Cache(storage,timeToLiveSeconds=ttl)
         data = fc.fetch(feed)
         for entry in data.entries:
             try:
                 url = unicode(entry.link, channels.encoding)
                 summary = unicode(entry.description, channels.encoding)
                 title = unicode(entry.title, channels.encoding)
-                #datepub = unicode(entry.date, channels.encoding)
             except:
                 url = entry.link
                 summary = entry.description
                 title = entry.title
-                #datepub = entry.date
         
-                ret.append( { 'url' : url, 'summary' : summary, 'title' : title , 'date' : datepub } )
+            date = entry.updated_parsed
+            datestr = time.strftime('%d/%m/%Y',date)
+            ret.append( { 'url' : url, 'summary' : summary, 'title' : title , 'date' : datestr } )
         
     finally:
         storage.close()
